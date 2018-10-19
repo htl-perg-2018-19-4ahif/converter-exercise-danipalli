491196ae16a749b1af6fde383068b05cf1263251
Daniel Pallinger <dani.pallinger@gmail.com>
2018-10-19 11:04:56 +0200

    Added Homework in less than 10 lines of code ;)

--- convert.js
+++ convert.js
@@ -1,1 +1,10 @@
-console.log('Not implemented yet!');
+const convert = require('convert-units')
+
+try {
+    if(isNaN(process.argv[2]) || process.argv[4] !== "to"){
+        throw exception; //should cause an error
+    }
+    console.log(process.argv[2]+" "+process.argv[3]+" are "+convert(process.argv[2]).from(process.argv[3]).to(process.argv[5])+" "+process.argv[5]);
+} catch (error) {
+    console.log('Invalid parameters');
+}
